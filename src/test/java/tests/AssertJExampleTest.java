df303e831ebf6ead5296a5342adf6307da5edff2
Brian Keenan <bkeenan@Excellus.com>
2024-01-18 16:15:11 -0500

    Added hamcrest and assertJ examples

---
+++ src/test/java/tests/AssertJExampleTest.java
@@ -0,0 +1,78 @@
+package tests;
+
+import animals.AnimalType;
+import animals.petstore.pet.attributes.Breed;
+import animals.petstore.pet.attributes.Gender;
+import animals.petstore.pet.attributes.Skin;
+import animals.petstore.pet.types.Dog;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test class example to show the power of assertJ
+ */
+public class AssertJExampleTest
+{
+    private List dListActual = Arrays.asList(
+            new Dog(AnimalType.DOMESTIC, Skin.FUR, Gender.MALE, Breed.MALTESE,
+                    new BigDecimal("750.00"), 1),
+            new Dog(AnimalType.DOMESTIC, Skin.FUR, Gender.MALE, Breed.POODLE,
+                    new BigDecimal("750.00"), 2),
+            new Dog(AnimalType.DOMESTIC, Skin.HAIR, Gender.FEMALE, Breed.GERMAN_SHEPARD,
+                    new BigDecimal("750.00"), 2));
+
+    private List dListExpected = Arrays.asList(
+            new Dog(AnimalType.DOMESTIC, Skin.FUR, Gender.MALE, Breed.MALTESE,
+                    new BigDecimal("750.00"), 1),
+            new Dog(AnimalType.DOMESTIC, Skin.FUR, Gender.MALE, Breed.POODLE,
+                    new BigDecimal("750.00"), 2),
+            new Dog(AnimalType.DOMESTIC, Skin.HAIR, Gender.FEMALE, Breed.CARDINAL,
+                    new BigDecimal("750.00"), 2)
+    );
+    @Test
+    @DisplayName("ABC test")
+    public void abcTest()
+    {
+        assertThat("abc").isEqualTo(123);
+
+    }
+    @Test
+    @DisplayName("Empty String test")
+    public void emptyStringTest()
+    {
+        assertThat("".isEmpty()).isTrue();
+
+    }
+    @Test
+    @DisplayName("Collection Test not null and not empty")
+    public void dogCollectionTest()
+    {
+       assertThat(dListActual)
+               .isNotEmpty()
+               .doesNotContainNull();
+    }
+
+    @Test
+    @DisplayName("Dog Collection  Match Tests1")
+    public void dogCollectionMatch()
+    {
+        assertThat(dListActual).isEqualTo(dListExpected)
+                .isNotEmpty()
+                .doesNotContainNull();
+    }
+
+    @Test
+    @DisplayName("Dog Collection  Match Tests2")
+    public void dogCollectionSameListTest()
+    {
+        assertThat(dListActual).isEqualTo(dListActual)
+                .isNotEmpty()
+                .doesNotContainNull();
+    }
+}
---
+++ src/test/java/tests/HamcrestExampleTest.java
@@ -0,0 +1,81 @@
+package tests;
+
+import animals.AnimalType;
+import animals.petstore.pet.attributes.Breed;
+import animals.petstore.pet.attributes.Gender;
+import animals.petstore.pet.attributes.Skin;
+import animals.petstore.pet.types.Dog;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Provide a hamcrest asserts example
+ */
+public class HamcrestExampleTest {
+    private List dListActual = Arrays.asList(
+            new Dog(AnimalType.DOMESTIC, Skin.FUR, Gender.MALE, Breed.MALTESE,
+                    new BigDecimal("750.00"), 1),
+            new Dog(AnimalType.DOMESTIC, Skin.FUR, Gender.MALE, Breed.POODLE,
+                    new BigDecimal("750.00"), 2),
+            new Dog(AnimalType.DOMESTIC, Skin.HAIR, Gender.FEMALE, Breed.GERMAN_SHEPARD,
+                    new BigDecimal("750.00"), 2));
+
+    private List dListExpected = Arrays.asList(
+            new Dog(AnimalType.DOMESTIC, Skin.FUR, Gender.MALE, Breed.MALTESE,
+                    new BigDecimal("750.00"), 1),
+            new Dog(AnimalType.DOMESTIC, Skin.FUR, Gender.MALE, Breed.POODLE,
+                    new BigDecimal("750.00"), 2),
+            new Dog(AnimalType.DOMESTIC, Skin.HAIR, Gender.FEMALE, Breed.CARDINAL,
+                    new BigDecimal("750.00"), 2)
+    );
+    @Test
+    @DisplayName("ABC test")
+    public void abcTest()
+    {
+        assertThat("abc",equalToIgnoringCase("b"));
+
+    }
+
+    @Test
+    @DisplayName("Empty String test")
+    public void emptyStringTest()
+    {
+        assertThat("",is(nullValue()));
+
+    }
+    @Test
+    @DisplayName("Collection Test not empty")
+    public void dogCollectionNotEmptyTest()
+    {
+        assertThat(dListActual.isEmpty(), is(false));
+
+    }
+    @Test
+    @DisplayName("Collection Test not null")
+    public void dogCollectionNotEmptyNotNull()
+    {
+        assertThat(dListActual, is(nullValue()));
+    }
+    @Test
+    @DisplayName("Dog Collection  Match Tests1")
+    public void dogCollectionMatch()
+    {
+        assertThat("List equality without order",dListExpected, equalTo(dListActual)) ;
+    }
+
+    @Test
+    @DisplayName("Dog Collection  Match Tests2")
+    public void dogCollectionSameListTest()
+    {
+        assertThat("List equality without order",dListExpected, equalTo(dListExpected)) ;
+
+    }
+}
